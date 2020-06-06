02da2e423f6b232d2e2cade04ba984a9b799d1bd
jeremyolu <jeremy-olu@hotmail.com>
2020-06-06 16:22:02 +0100

    updated authors table with profileimage

--- TopBloggers/Models/Author.cs
+++ TopBloggers/Models/Author.cs
@@ -25,6 +25,7 @@ namespace TopBloggers.Models
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+        public string ProfileImage { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Article> Articles { get; set; }
